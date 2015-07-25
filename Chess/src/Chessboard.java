7282df0aa7d4ed378bff3992d7178d935d04c2ea
Johannes Sebastian Veit <johannessebastian.veit@hs-augsburg.de>
2015-07-25 22:34:35 +0200

    Das Schachbrett an sich:
    Wir wollen uns nun daran machen, unseren Entwurf in konkreten Java-Code
    umzusetzen.
    Wir beginnen mit der Schachbrett/Schachfiguren-Konstellation und
    realisieren das Interface Chessman

---
+++ Chess/src/Chessboard.java
@@ -0,0 +1,110 @@
+
+/**
+ * 
+ * @author Sebastian
+ *
+ * @Erklaerung
+ * Diese Klasse repraesentiert ein Schachbrett, auf dem sich Figuren
+ * befinden. Diese Figuren duerfen durch entsprechende Befehle
+ * bewegt werden.
+ **/
+
+public class Chessboard {
+	
+	/** In diesem Array wird die aktuelle Belegung des Schachbretts 
+	 * gespeichert
+	 * 
+	 **/
+	
+	protected Chessman[] [] content;
+	
+	/** Konstruktor
+	 * @param rows die Anzahl der Zeilen
+	 * @param cols die Anzahl der Spalten
+	 **/
+	
+	public Chessboard(int rows, int cols){
+		content = new Chessman[rows] [cols];
+	}
+	
+	public boolean isValid(int row, int col) {
+		return row >=0 && col >= 0 &&
+				row < content.length && col < content[0].length;
+	}
+	
+	/** Gibt die Anzahl der Spalten des Schachbretts zurueck */
+	public int getNumOfColumns() {
+		return content[0].length;
+	}
+	
+	/**Gibt die Anzahl der Zeilen des Schachbretts zuerueck */
+	public int getNumOfRows() {
+		return content.length;
+	}
+	
+	/** Gibt den Inhalt des Bretts an einer gegebenen Position an.
+	 * @param row der Zeilenindex, gezaehlt ab Index 0
+	 * @param col der Spaltenindex, gezaehlt ab Index 0
+	 * @return den Inhalt des Bretts an der gegebenen Position,
+	 * 	null, wenn dort keine Figur steht
+	 **/
+	
+	public Chessman getContent(int row, int col) {
+		return content[row][col];
+	}
+	
+	/** Besetzt ein Feld mit einer Spielfigur. Diese Methode ueberprueft
+	 * nicht, ob der Zug gemaess den Schachregeln erlaubt ist.
+	 * @param chessman die zu setzende Spielfigur oder null
+	 * @param row der Zeilenindex, gezaehlt ab Index 0
+	 * @param col der Spaltenindex, gezaehlt ab Index 0
+	 * @return den Inhalt, der zuvor auf diesem Feld war.
+	 **/
+	
+	public Chessman getContent(int row, int col, Chessman chessman) {
+		Chessman result = content[row][col];
+		content[row][col] = chessman;
+		return result;
+	}
+	
+	public boolean equals(Object o) {
+		if (o instanceof Chessboard) {
+			Chessboard board = (Chessboard) o;
+			//Vergleiche die Dimensionen der Felder
+			if(board.content.length != content.length ||
+				board.content[0].length != content[0].length){
+					return false;
+				}
+			//Ueberpruefe die Feldinhalte
+			for(int i = 0; i < content.length; i++){
+				for(int j= 0; j < board.content[i].length; j++){
+					Chessman man1 = content[i][j];
+					Chessman man2 = board.content[i][j];
+					if(man1 == null && man2 == null)
+						continue;
+					if(man1 != null && man2 != null && man1.equals(man2))
+						continue;
+					return false;
+				}
+			}
+			return true;
+			}
+		return false;
+		}
+	
+	/**
+	 * Gibt den Hashcoe dieses Objektes zurueck. Wer die
+	 * equels-Methode ueberschreibt, muss auch immer die
+	 * hashCode-Methode anpassen!
+	 */
+	public int hashCode() {
+		int res = 0;
+		for(int i = 0; i < content.length; i++)
+			for(int j = 0; j < content[i].length; j++)
+				if(content[i][j] != null)
+					res += content[i][j].hashCode();
+		return res;
+	}
+	
+	
+	}
--- Chess/src/Chessman.java
+++ Chess/src/Chessman.java
@@ -1,4 +1,58 @@
 
-public interface Chessman {
+/**
+ * 
+ * @author Sebastian
+ *
+ * @Erklaerung
+ * Dieses Interface repraesentiert eine beliebige Figur,
+ * die auf ein Schachbrett gestellt werden kann. Das Interface
+ * beinhaltet hierbei keine Positionsangabe auf dem Brett, d.h.
+ * Instanzen dieseses Interfaces koennen mehrmals auf demselben
+ * Brett bzw. auf mehreren Brettern zugleich verwendet werden. Sie
+ * repraesentieren somit lediglich den Typ einer Schafigur
+ * (also Laeufer, Springer usw).
+ */
 
+public interface Chessman {
+	
+	/** Liefert ein einzelnes Zeichen, das den Figurentyp
+	 * repraesentiert. Diese Methode kann fuer einfache textuelle
+	 * Darstellungen verwedent werden.
+	 **/
+	public char toChar();
+	
+	/** Testet, ob diese Figur eine andere Figur theoretisch zu schlagen
+	 * in der Lage ist.
+	 * @param die Schachfigur, die es zu ueberpruefen gilt
+	 * @return true genau dann, wenn die Figur geschlagen werden kann
+	 **/
+	
+	public boolean canTake(Chessman chessman);
+	
+	/** Ueberprueft, auf welche Figur sich die Fugr im naechsten Zug
+	 * theoretisch bewegen kann.
+	 * @return ein Booleschen Feld in der Dimension des Schachbretts.
+	 * Ein Eintrag ist genau dann true wenn die Figur auf dieses Feld
+	 * ziehen darf.
+	 * @param board das Schachbrett, auf dem agiert wird
+	 * @param row die Nummer der Zeile , in der sich die Figur befindet.
+	 * 		Die Methode muss nicht ueberpruefen, ob unter der angegebenen
+	 * 		Position wirklich eine entsprechende Figur steht. Die Nummer
+	 * 		wird von 0 an gezaehlt
+	 * @param col die Nummer der Spalte, in der sich die Figur
+	 * 		befindet.
+	 * 		Die Methode muss nicht ueberpruefen, ob unter der angegebenen
+	 * 		Position wirklich eine entsprechende Figur steht. Die Nummer
+	 * 		wird von 0 an gezaehlt
+	 **/
+	
+	public boolean[] [] getDestinations
+		(Chessboard board, int row, int col);
+	
+	/** Ueberprueft, ob es sich bei zwei Schachfiguren um dieselbe Figur
+	 * handelt.
+	 * @return true genau dann, wenn es sich um dieselbe Figur handelt
+	 **/
+	
+	public boolean equals(Object o);
 }
