c8edbaeff6006722db48f0a3e0abecbdcd32e46c
Johannes Sebastian Veit <johannessebastian.veit@hs-augsburg.de>
2015-07-25 19:33:04 +0200

    Neuer Commit Eingabe und Chessman

---
+++ Chess/src/EingabeTest.java
@@ -0,0 +1,9 @@
+import Prog1Tools.IOTools;
+
+
+public class EingabeTest {
+	public static void main(String[] args) {
+		int i = IOTools.readInt("Geben Sie ein int zahl ein");
+		System.out.println("Eingebene Zahl:"+i);
+	}
+}
