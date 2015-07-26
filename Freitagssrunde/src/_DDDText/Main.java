9913dcc526759a1bb020e1ecd41d0fd349d6ec0a
Johannes Sebastian Veit <johannessebastian.veit@hs-augsburg.de>
2015-07-26 12:36:06 +0200

    DDDText aus der Seite: http://wiki.freitagsrunde.org/Javakurs2009/3dText#Beispiel

--- Freitagssrunde/src/_DDDText/Main.java
+++ Freitagssrunde/src/_DDDText/Main.java
@@ -1,10 +1,32 @@
 package _DDDText;
 
 public class Main {
+	
+	private static DDDText text = new DDDText();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		String hText = new String("HALLOWELT");
+		
+		char[] hTextA = hText.toCharArray();
+		
+		for(int i = 0; i < hTextA.length; i++){
+//			System.out.println(hTextA[i] - ('A'));
+			int wert = hTextA[i] - ('A');
+			for(int j = 0; j < text.letters[wert].length; ++j){
+				System.out.println(text.letters[wert][j]);
+			}
+		}
+		
+		for(int i = 0; i < text.letters.length; ++i){
+			System.out.println("neuer letter i= " + i);
+			for(int j = 0; j < text.letters[i].length; ++j){
+				System.out.println(text.letters[i][j]);
+			}
+		}
+		
+		
 	}
 
 }
